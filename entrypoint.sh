6e5dc26e4632cd37ed0f0cdea582375cde4004c9
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2020-03-19 21:31:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,4 @@
 set -e
 set -x
 
-# There was only one argument, call tectonic directly
-if [ -z "$2" ]; then
-    tectonic $1
-else
-    # Optionally, we install the packages required by the build.
-    nix-env -f "<nixpkgs>" -iA $1
-
-    # Compile the document
-    tectonic $2
-fi
+tectonic $1
